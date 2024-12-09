14159fe9c49e6a690615758d96555837e09bde83
ian1506 <akm.king1565@gmail.com>
2024-12-09 18:03:18 +0700

    Add files via upload

---
+++ js/processAppointment.js
@@ -0,0 +1,10 @@
+/*
+ * Author: Johann Olivares
+ * File: processAppointment.js
+ * Date: 06/01/2021
+ * Description: This script houses the logic that
+ *              will process and store appoitments.
+ *  
+ */
+
+//Currently under construction...
